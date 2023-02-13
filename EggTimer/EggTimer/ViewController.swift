d283910bf682aa987411940e5dc54e59afa6f3fa
보경 <97583162+bokoo14@users.noreply.github.com>
2023-02-13 17:04:50 +0900

    feat: progressBar, titleLabel, timer

--- EggTimer/EggTimer/ViewController.swift
+++ EggTimer/EggTimer/ViewController.swift
@@ -9,30 +9,42 @@
 import UIKit
 
 class ViewController: UIViewController {
-    let eggTimes = ["Soft": 300, "Medium": 420, "Hard": 720]
+    @IBOutlet weak var progressBar: UIProgressView! // view the progress percent
+    @IBOutlet weak var titleLabel: UILabel!  // change the label
     
+    //egg timer
+    let eggTimes = ["Soft": 3, "Medium": 4, "Hard": 7]
+    var timer = Timer() // when the button pressed more than twice-> reset the timer and create new timer
+    
+    var totalTime = 0
+    var secondPassed = 0
+    
+    // when pressed the wanted button, egg timer start!
     @IBAction func hardnessSelected(_ sender: UIButton) {
-        var hardness = sender.currentTitle! //Soft, Medium, Hard
+        //Stops the timer from ever firing again and requests its removal from its run loop.
+        timer.invalidate()
         
-         let result = eggTimes[hardness]!
-        print(result)
+        let hardness = sender.currentTitle! //Soft, Medium, Hard
+        totalTime = eggTimes[hardness]!
+        
+        progressBar.progress = 0.0
+        secondPassed = 0
+        titleLabel.text = "\(hardness) egg"
+        
+        print("this counter counts seconds: \(secondPassed)")
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateCounter), userInfo: nil, repeats: true)
     }
     
-    var counter = 60
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateCounter), userInfo: nil, repeats: true)
-    }
-
+    // counts the time
     @objc func updateCounter() {
-        //example functionality
-        if counter > 0 {
-            print("\(counter) seconds to the end of the world")
-            counter -= 1
+        //count the passed time until totalTime
+        if secondPassed < totalTime {
+            secondPassed += 1
+            progressBar.progress = Float(secondPassed)/Float(totalTime)
+        }else{ // when the time becomes 0
+            timer.invalidate() // stops the timer
+            titleLabel.text = "DONE!"
         }
     }
-
 
 }
