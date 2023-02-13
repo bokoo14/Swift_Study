e0f87bf03824562beb3cfb4cd7022a2e14def915
보경 <97583162+bokoo14@users.noreply.github.com>
2023-02-13 17:59:42 +0900

    feat: 알람 기능

--- EggTimer/EggTimer/ViewController.swift
+++ EggTimer/EggTimer/ViewController.swift
@@ -7,35 +7,42 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
     @IBOutlet weak var progressBar: UIProgressView! // view the progress percent
     @IBOutlet weak var titleLabel: UILabel!  // change the label
     
-    //egg timer
+    // egg timer
     let eggTimes = ["Soft": 3, "Medium": 4, "Hard": 7]
-    var timer = Timer() // when the button pressed more than twice-> reset the timer and create new timer
+    var timer = Timer() // when the button pressed -> create new timer
     
+    // count the seconds
     var totalTime = 0
     var secondPassed = 0
     
-    // when pressed the wanted button, egg timer start!
+    // play the audio that saved in my proj
+    var player: AVAudioPlayer!
+    
+    // when pressed the button, egg timer starts
     @IBAction func hardnessSelected(_ sender: UIButton) {
         //Stops the timer from ever firing again and requests its removal from its run loop.
         timer.invalidate()
         
-        let hardness = sender.currentTitle! //Soft, Medium, Hard
-        totalTime = eggTimes[hardness]!
+        let hardness = sender.currentTitle! //save the button title -> Soft, Medium, Hard
+        totalTime = eggTimes[hardness]! // 3, 4, 7
         
+        //reset the existing progress and time & rename the titleLabel
         progressBar.progress = 0.0
         secondPassed = 0
         titleLabel.text = "\(hardness) egg"
         
+        // timer
         print("this counter counts seconds: \(secondPassed)")
         timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateCounter), userInfo: nil, repeats: true)
     }
     
-    // counts the time
+    // counts the time func
     @objc func updateCounter() {
         //count the passed time until totalTime
         if secondPassed < totalTime {
@@ -44,6 +51,11 @@ class ViewController: UIViewController {
         }else{ // when the time becomes 0
             timer.invalidate() // stops the timer
             titleLabel.text = "DONE!"
+            
+            // play the audio that saved in my proj file
+            let url = Bundle.main.url(forResource: "alarm_sound", withExtension: "mp3")
+            player = try! AVAudioPlayer(contentsOf: url!)
+            player.play()
         }
     }
 
