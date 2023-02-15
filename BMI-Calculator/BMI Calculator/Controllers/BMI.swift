fc8dc731e1e2741cee39f17bfb636568d66276fd
보경 <97583162+bokoo14@users.noreply.github.com>
2023-02-15 20:55:59 +0900

    feat: segue and navigate for multi screen

---
+++ BMI-Calculator/BMI Calculator/Controllers/BMI.swift
@@ -0,0 +1,15 @@
+//
+//  BMI.swift
+//  BMI Calculator
+//
+//  Created by Bokyung on 2023/02/15.
+//  Copyright © 2023 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+struct BMI {
+    let value: Float
+    let advice: String
+    let color: UIColor
+}
