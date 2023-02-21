04bed633e31adf8e78a06f6d3ffa2e7245e15923
보경 <97583162+bokoo14@users.noreply.github.com>
2023-02-21 14:30:27 +0900

    feat: weather API

--- Clima/WeatherManager.swift
+++ Clima/WeatherManager.swift
@@ -45,7 +45,7 @@ struct WeatherManager {
         let decoder = JSONDecoder()
         do{
             let decodedData = try decoder.decode(WeatherData.self, from: weatherData)
-            print(decodedData.weather[0].description)
+            print(decodedData.weather[0].id)
         } catch {
             print(error)
         }
