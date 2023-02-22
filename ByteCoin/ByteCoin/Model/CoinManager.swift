e72061449661f58ae1c04c4248571c0e1c711049
보경 <97583162+bokoo14@users.noreply.github.com>
2023-02-22 16:26:20 +0900

    feat: connect bitcoin API

--- ByteCoin/ByteCoin/Model/CoinManager.swift
+++ ByteCoin/ByteCoin/Model/CoinManager.swift
@@ -11,9 +11,23 @@ import Foundation
 struct CoinManager {
     
     let baseURL = "https://rest.coinapi.io/v1/exchangerate/BTC"
-    let apiKey = "YOUR_API_KEY_HERE"
+    let apiKey = "553ACAE8-62E4-4164-994E-0AC4C4EA02B6"
     
     let currencyArray = ["AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]
-
     
+    // for: external name, currency: internal name
+    func getCoinPrice(for currency: String){
+        // pass the selected currency to the CoinManager
+        
+        // Use String concatenation to add the selected currency at the end of the baseURL along with the API key
+        let urlString = "\(baseURL)/\(currency)?apikey=\(apiKey)"
+        
+        // use optional binding to unwrap the URL that's created from the urlString
+        if let url = URL(string: urlString){
+            // create a new URLSession object with default configuration
+            let session = URLSession(configuration: .default)
+            
+        }
+    }
+
 }
