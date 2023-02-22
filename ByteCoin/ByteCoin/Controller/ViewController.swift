b84535cac6c04bc1025a73dd44f69714d6a34206
보경 <97583162+bokoo14@users.noreply.github.com>
2023-02-22 22:51:33 +0900

    feat: upload user interface

--- ByteCoin/ByteCoin/Controller/ViewController.swift
+++ ByteCoin/ByteCoin/Controller/ViewController.swift
@@ -9,24 +9,26 @@
 import UIKit
 
 class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate, CoinManagerDelegate {
+   
+    
     
     @IBOutlet weak var bitcoinLabel: UILabel!
     @IBOutlet weak var currencyLabel: UILabel!
     @IBOutlet weak var currencyPicker: UIPickerView!
     
-    let coinManager = CoinManager()
+    // need to change this to a var to be able to modify its properties
+    var coinManager = CoinManager()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        // easily missed: must set the coinManager's delegate as this current class so that we can receive the notification when the delegate methods are called
+        coinManager.delegate = self
+        
         
         // set the datasource for the picker
         currencyPicker.dataSource = self
         // to update the PickerView with some titles and detect when it is interacted with
         currencyPicker.delegate = self
-
-        
-        // easily missed: must set the coinManager's delegate as this current class so that we can receive the notification when the delegate methods are called
-        coinManager.delegate = self
     }
     
     
@@ -41,6 +43,7 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
         return coinManager.currencyArray.count
     }
     
+    
     //MARK: - UIPickerViewDelegate
     // return the title for a given row
     func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
@@ -52,6 +55,22 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
         // pass the selected currency to the CoinManager via the getCoinPrice
         let selectedCurrency = coinManager.currencyArray[row]
         coinManager.getCoinPrice(for: selectedCurrency)
+    }
+    
+    
+    //MARK: - CoinManagerDelegate
+    // provide the implement for the delegate methods
+    // when the coinManager gets the price it will call this method and pass over the price and currency
+    func didUpdatePrice(price: String, currency: String) {
+        // Rememeder that we need to get hold of the main thread to update the UI, otherwise our app will crash if we try to do this from a background thread (URLSession works in the background)
+        DispatchQueue.main.async {
+            self.bitcoinLabel.text = price
+            self.currencyLabel.text = currency
+        }
+    }
+    
+    func didFailWithError(error: Error) {
+        print(error)
     }
     
 }
