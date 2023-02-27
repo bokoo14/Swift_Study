658bcd129e068466127cf9c0506390ba9ae46f29
보경 <97583162+bokoo14@users.noreply.github.com>
2023-02-27 18:04:16 +0900

    feat: ChatVIewController table view

---
+++ Flash-Chat/Flash Chat iOS13/Message.swift
@@ -0,0 +1,14 @@
+//
+//  Message.swift
+//  Flash Chat iOS13
+//
+//  Created by Bokyung on 2023/02/27.
+//  Copyright © 2023 Angela Yu. All rights reserved.
+//
+
+import Foundation
+
+struct Message {
+    let sender: String
+    let body: String
+}
