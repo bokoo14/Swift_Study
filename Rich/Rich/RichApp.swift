8ff92f4a0ab7c64e41be04b9809e5e18b5dc99a0
보경 <97583162+bokoo14@users.noreply.github.com>
2023-03-15 00:12:53 +0900

    build: SwiftUI

---
+++ Rich/Rich/RichApp.swift
@@ -0,0 +1,17 @@
+//
+//  RichApp.swift
+//  Rich
+//
+//  Created by Bokyung on 2023/03/14.
+//
+
+import SwiftUI
+
+@main
+struct RichApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
