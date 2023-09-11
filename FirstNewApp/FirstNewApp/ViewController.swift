b257b46712c0ade2681275501aedc78f1c9a91fa
보경 <97583162+bokoo14@users.noreply.github.com>
2023-09-11 22:20:25 +0900

    create new project

---
+++ FirstNewApp/FirstNewApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FirstNewApp
+//
+//  Created by Bokyung on 2023/09/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
