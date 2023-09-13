ff388ca39e68e178db5240e7f6e6375ef79c0ff3
보경 <97583162+bokoo14@users.noreply.github.com>
2023-09-13 23:13:57 +0900

    [Feat] 버튼 및 컴포넌트 연결

--- FirstNewApp/FirstNewApp/ViewController.swift
+++ FirstNewApp/FirstNewApp/ViewController.swift
@@ -9,11 +9,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var mainLabel: UILabel!
+    
+    
+    
+    
+    // 재정의, 앱의 화면에 들어오면 처음 실행
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        configureUI()
+    }
+    
+    func configureUI() {
+        mainLabel.text = "초를 선택하세요"
     }
 
-
+    
+    // 값이 변할때마다 함수를 호출
+    @IBAction func sliderChanged(_ sender: UISlider) {
+        
+    }
+    
+    
+    @IBAction func resetButtonTapped(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func startButtonTapped(_ sender: UIButton) {
+    }
+    
 }
 
